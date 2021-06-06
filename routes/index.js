ce582437eaa3eec47e5bd0c6c8202f06968558d0
Martin Gutsche <mgutsche@pm.me>
2021-06-06 15:14:54 +0200

    Remove lecturesRouter

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 import express from 'express'
-import lecturesRouter from './lectures.js'
 import usersRouter from './users.js'
 
 const router = express.Router()
 
-// router.use('/lectures', keycloak.protect(protectByUserId), lecturesRouter)
 router.use('/users', usersRouter)
 
 export default router
